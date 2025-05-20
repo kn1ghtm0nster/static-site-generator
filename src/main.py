8c5446fe3ded79fd52894c6c363aeb6d2b50df48
kn1ghtm0nster <diego.quintanilla@proton.me>
2025-05-19 22:00:33 -0400

    integrate generate_page function with main func

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 from textnode import TextNode
-from helpers import copy_static
+from helpers import copy_static, generate_page
 
 
 def main():
     copy_static("static", "public")
+
+    generate_page(from_path="content/index.md",
+                  template_path="template.html", dest_path="public/index.html")
 
 
 if __name__ == "__main__":
